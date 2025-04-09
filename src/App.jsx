eb8f68920a4ba9be57f70f0d329ddf3945c2684d
Tushar <tusharvarshney1810@gmail.com>
2025-04-09 22:36:13 +0530

    intial conflec

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
 import Contact from "./components/contact/contact";
 import About from "./components/About/About";
